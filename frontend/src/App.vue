a7650e009ab761aeaf3c74c2562e3b429eff9c62
진해진 <j__0308@naver.com>
2021-09-14 15:39:58 +0900

    [S05P21C201-117][FE] init : frontend 초기 설정

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,36 +1,30 @@
 <template>
-  <div>
-    <!-- <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div> -->
-    <router-view />
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
   </div>
+  <router-view />
 </template>
 
-<style>
+<style lang="scss">
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #292321;
+  color: #2c3e50;
 }
 
 #nav {
   padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  a {
+    font-weight: bold;
+    color: #2c3e50;
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-
-.form-group .content {
-  font-size: 1.2rem;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,45 +1,18 @@
 <template>
-  <div>
-    <h-nav></h-nav>
-    <div id="main-overview" class="container">
-      <div class="row">
-        <div class="col-md-12">
-          <h1>DCOMMERCE PLATFORM</h1>
-          <h4 class="mt-5">
-            블록체인 기반의 에스크로를 사용하는 전자상거래 플랫폼을 이용해보세요!
-          </h4>
-          <router-link
-            :to="{ name: 'signup' }"
-            class="btn btn-lg btn-primary mt-5"
-            v-if="!$store.state.isSigned"
-          >
-            회원가입
-          </router-link>
-          <router-link
-            :to="{ name: 'shop' }"
-            class="btn btn-lg btn-primary mt-5"
-            v-if="$store.state.isSigned"
-          >
-            Shop
-          </router-link>
-        </div>
-      </div>
-    </div>
-    <step-flow></step-flow>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
-<script>
-import HNav from "../components/common/HNav";
-import StepFlow from '@/components/common/StepFlow';
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
-export default {
+export default defineComponent({
+  name: "Home",
   components: {
-    HNav,
-    StepFlow
-  }
-};
+    HelloWorld,
+  },
+});
 </script>
-
-<style>
-</style>
