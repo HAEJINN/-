a8d7f0affd9f5174bce042d9309b11168f9d8e6a
진해진 <j__0308@naver.com>
2021-09-15 22:39:46 +0900

    [S05P21C201-117][FE] fix : 디렉토리 구조 정리

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -2,7 +2,7 @@
 // import state from "./state";
 // import * as getters from "./getters";
 // import * as mutations from "./mutations";
-import * as actions from "./actions";
+import * as actions from "./actions/userActions";
 
 const root = {
   namespaced: true,
