b796debbb41a6a6487b3b4154dcebf48b7414ceb
진해진 <j__0308@naver.com>
2021-09-15 15:10:56 +0900

    [S05P21C201-161][FE] feat : nav bar 생성

---
+++ frontend/src/views/main/components/nav.vue
@@ -0,0 +1,51 @@
+<template>
+  <q-layout view="hHh LpR lFf">
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+        <q-toolbar-title> 나만의 작은 화가 </q-toolbar-title>
+        <q-btn flat round dense icon="sim_card" class="q-mr-xs" />
+        <q-btn flat round dense icon="gamepad" />
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer v-model="leftDrawerOpen" side="left" bordered>
+      <div class="q-pa-sm">
+        <q-list>
+          <q-item clickable>
+            <q-item-section>Menu1</q-item-section>
+          </q-item>
+          <q-separator />
+          <q-item clickable>
+            <q-item-section>Menu2</q-item-section>
+          </q-item>
+          <q-separator />
+          <q-item clickable>
+            <q-item-section>Menu3</q-item-section>
+          </q-item>
+          <q-separator />
+          <q-item clickable>
+            <q-item-section>Menu4</q-item-section>
+          </q-item>
+        </q-list>
+      </div>
+    </q-drawer>
+  </q-layout>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const leftDrawerOpen = ref(false);
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+    };
+  },
+};
+</script>
--- frontend/src/views/main/main.vue
+++ frontend/src/views/main/main.vue
@@ -1,14 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="@/assets/logo.png" />
-    나만의 작은 화실
+  <div class="coloum">
+    <!-- nav bar -->
+    <Nav></Nav>
+    <div class="col-sm-12 col-md-8">메인 베너입니다</div>
+    <div class="col-sm-12 col-md-8">전시회 리스트 입니다</div>
+    <div class="col-sm-12 col-md-8">최근 등록 작가입니다</div>
+    <div class="col-sm-12 col-md-8">달력입니다</div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import Nav from "@/views/main/components/nav.vue";
 
 export default defineComponent({
   name: "Main",
+  components: {
+    Nav,
+  },
 });
 </script>
