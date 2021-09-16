b98a411243d5c2c0a93df54fbb7bf44b675a0504
진해진 <j__0308@naver.com>
2021-09-16 15:52:08 +0900

    [S05P21C201-168][FE] feat : 메인 - 달력 생성

---
+++ frontend/src/views/main/components/calendar.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="q-pa-md row justify-center">
+    <q-date v-model="date" minimal class="col-xs-auto col-xl-6" />
+  </div>
+</template>
--- frontend/src/views/main/main.vue
+++ frontend/src/views/main/main.vue
@@ -4,7 +4,7 @@
     <Exhibition></Exhibition>
     <Item></Item>
     <div class="col-sm-12 col-md-8">신규 작가입니다</div>
-    <div class="col-sm-12 col-md-8">달력입니다</div>
+    <Calendar></Calendar>
   </div>
 </template>
 
@@ -13,12 +13,14 @@ import { defineComponent } from "vue";
 import Banner from "@/views/main/components/banner.vue";
 import Item from "@/views/main/components/item.vue";
 import Exhibition from "@/views/main/components/exhibition.vue";
+import Calendar from "@/views/main/components/calendar.vue";
 export default defineComponent({
   name: "Main",
   components: {
     Banner,
     Exhibition,
     Item,
+    Calendar,
   },
 });
 </script>
