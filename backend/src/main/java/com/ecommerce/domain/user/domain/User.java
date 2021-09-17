f85ffd7efc286f4cd142da4eadd9902af955a48c
kwj1270 <kwj1270@naver.com>
2021-09-17 17:40:27 +0900

    hotfix: oauth2.0 login

--- backend/src/main/java/com/ecommerce/domain/user/domain/User.java
+++ backend/src/main/java/com/ecommerce/domain/user/domain/User.java
@@ -1,68 +1,107 @@
 package com.ecommerce.domain.user.domain;
 
-import com.ecommerce.domain.exception.DomainException;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import org.apache.logging.log4j.util.Strings;
+import com.ecommerce.domain.BaseTimeEntity;
+import com.ecommerce.domain.photo.domain.Photo;
+import com.ecommerce.domain.user.domain.vo.SocialType;
+import com.ecommerce.domain.user.domain.vo.UserRole;
+import com.ecommerce.domain.user.domain.vo.UserStatus;
+import lombok.*;
 
 import javax.persistence.*;
+import java.util.Objects;
 
+@ToString
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "users")
 @Entity
-public class User {
+public class User extends BaseTimeEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
 
+    @Column(name = "user_name")
     private String name;
+
+    @Column(name = "user_email")
     private String email;
-    private String password;
+
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "photo_id")
+    private Photo photo;
+
+    @Column(name = "user_commnet")
+    private String comment;
+
+    @Column(name = "user_description")
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "user_type")
+    private UserRole userRole;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "user_social_type")
+    private SocialType socialType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "user_status")
+    private UserStatus status;
 
     @Builder
-    public User(String name, String email, String password) {
+    public User(final String name, final String email, final Photo photo,
+                final String comment, final String description, final UserRole userRole,
+                final SocialType socialType, final UserStatus status) {
         this.name = name;
         this.email = email;
-        this.password = password;
+        this.photo = photo;
+        this.comment = comment;
+        this.description = description;
+        this.userRole = userRole;
+        this.socialType = socialType;
+        this.status = status;
     }
 
-    public User update(final User other) {
-        return changeName(other.name)
-                .changePassword(other.password)
-                .changeEmail(other.email);
-    }
-
-    public void matchPassword(final String password) {
-        if (!this.password.equals(password)){
-            throw new DomainException("비밀번호가 일치하지 않습니다.");
-        }
+    public User update(final String name, final String email, final String photo, final SocialType socialType) {
+        return changeName(name)
+                .changeEmail(email)
+                .changePhoto(photo)
+                .changeSocialType(socialType);
     }
 
     public User changeName(final String name) {
-        validateStringBlank(name);
+        validateNull(photo);
         this.name = name;
         return this;
     }
 
     public User changeEmail(final String email) {
-        validateStringBlank(email);
+        validateNull(photo);
         this.email = email;
         return this;
     }
 
-    public User changePassword(final String password) {
-        validateStringBlank(password);
-        this.password = password;
+    public User changePhoto(final String photo) {
+        validateNull(photo);
+        this.photo = new Photo(photo);
         return this;
     }
 
-    private void validateStringBlank(final String sentence) {
-        if (Strings.isBlank(sentence)) {
+    public User changeSocialType(final SocialType socialType) {
+        validateNull(socialType);
+        this.socialType = socialType;
+        return this;
+    }
+
+    private void validateNull(final Object object) {
+        if(Objects.isNull(object)) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public String getUserRole() {
+        return userRole.name();
     }
 
 }
---
+++ backend/src/main/java/com/ecommerce/domain/user/domain/vo/SocialType.java
@@ -0,0 +1,5 @@
+package com.ecommerce.domain.user.domain.vo;
+
+public enum SocialType {
+    KAKAO,NAVER,GOOGLE
+}
