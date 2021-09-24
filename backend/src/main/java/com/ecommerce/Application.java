2aece50777300f0a272251e6e605864278f9490c
kwj1270 <kwj1270@naver.com>
2021-09-24 16:42:56 +0900

    feat: eth 연ê서버  íh server connection test

--- backend/src/main/java/com/ecommerce/Application.java
+++ backend/src/main/java/com/ecommerce/Application.java
@@ -3,12 +3,16 @@ package com.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
-
+import org.web3j.crypto.Credentials;
+import org.web3j.erc721.ERC721;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.tx.gas.DefaultGasProvider;
 
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SpringApplication app = new SpringApplication(Application.class);
         app.addListeners(new ApplicationPidFileWriter()); // pid 파일을 생성하는 writer 등록
         app.run(args);
---
+++ backend/src/test/java/com/ecommerce/SolTest.java
@@ -0,0 +1,43 @@
+package com.ecommerce;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.web3j.EVMTest;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.response.Web3ClientVersion;
+import org.web3j.protocol.http.HttpService;
+
+@EVMTest
+public class SolTest {
+
+    @DisplayName("getEthClientVersionSync")
+    @Test
+    public void getEthClientVersionSync() throws Exception
+    {
+        Web3j web3j = Web3j.build(new HttpService());
+        Web3ClientVersion web3ClientVersion = web3j.web3ClientVersion().send();
+        System.out.println(web3ClientVersion.getWeb3ClientVersion());
+    }
+
+    @DisplayName("getEthClientVersionASync")
+    @Test
+    public void getEthClientVersionASync() throws Exception
+    {
+        Web3j web3 = Web3j.build(new HttpService());  // defaults to http://localhost:8545/
+        Web3ClientVersion web3ClientVersion = web3.web3ClientVersion().sendAsync().get();
+        System.out.println(web3ClientVersion.getWeb3ClientVersion());
+    }
+
+    @DisplayName("getEthClientVersionRx")
+    @Test
+    public void getEthClientVersionRx() throws Exception
+    {
+        Web3j web3 = Web3j.build(new HttpService());  // defaults to http://localhost:8545/
+        web3.web3ClientVersion().flowable().subscribe(x -> {
+            System.out.println(x.getWeb3ClientVersion());
+        });
+
+        Thread.sleep(5000);
+    }
+
+}
