de18096833547c0998a5374c15d9f586f7d8a099
진해진 <j__0308@naver.com>
2021-09-28 04:18:19 +0900

    [S05P21C201][FE] fix : 로그인 스타일

--- frontend/src/views/login/dialog.vue
+++ frontend/src/views/login/dialog.vue
@@ -4,30 +4,34 @@
       view="lhh LpR lff"
       container
       style="height: 500px"
-      class="bg-grey-3"
+      class="bg-secondary"
     >
       <q-page-container>
-        <q-header class="bg-black">
+        <q-header class="bg-dark">
           <q-toolbar>
             <q-btn v-close-popup flat round dense icon="close" />
           </q-toolbar>
         </q-header>
-        <q-page class="">
-          <a href="/oauth2/authorization/google">
-            <img :src="google" />
-          </a>
-          <a href="/oauth2/authorization/kakao">
-            <img :src="kakao" />
-          </a>
-          <a href="/oauth2/authorization/naver">
-            <img :src="naver" />
-          </a>
-          <q-btn
-            color="black"
-            text-color="white"
-            label="login Lets git it~!"
-            @click="openDialogLoginSuccess"
-          />
+        <q-page class="column justify-center">
+          <div class="row">
+            <a
+              class="col login-image google"
+              href="/oauth2/authorization/google"
+            >
+            </a>
+            <a class="col login-image kakao" href="/oauth2/authorization/kakao">
+            </a>
+            <a class="col login-image naver" href="/oauth2/authorization/naver">
+            </a>
+          </div>
+          <div class="row justify-center">
+            <q-btn
+              color="black"
+              text-color="white"
+              label="로그인 성공^0^"
+              @click="openDialogLoginSuccess"
+            />
+          </div>
         </q-page>
       </q-page-container>
     </q-layout>
@@ -35,6 +39,7 @@
 </template>
 
 <script>
+import "../../styles/login.scss";
 import { reactive, ref } from "vue";
 
 export default {
@@ -44,14 +49,8 @@ export default {
       emit("loginSuccess");
     };
     return {
-      google: require("../../assets/login/google_line.png"),
-      kakao: require("../../assets/login/kakao_big.png"),
-      naver: require("../../assets/login/naver_green.png"),
-
       openDialogLoginSuccess,
     };
   },
 };
 </script>
-
-<style lang="ts"></style>
