0a337ba35602f11b823edeffbc76f4fbb306f350
진해진 <j__0308@naver.com>
2021-09-27 15:04:12 +0900

    [S05P21C201-194][FE] feat :  마이페이지-프로필이미지

--- frontend/src/views/mypage/components/profile.vue
+++ frontend/src/views/mypage/components/profile.vue
@@ -1,3 +1,16 @@
 <template>
-  <div>프로필공간</div>
+  <div class="row justify-center">
+    <div class="col-lg-2 col-xs-8">
+      <q-responsive :ratio="1">
+        <div class="profile-image">
+          <div class="modify-profile"></div>
+        </div>
+      </q-responsive>
+    </div>
+  </div>
 </template>
+<script lang="ts">
+import "../../../styles/mypage.scss";
+import { defineComponent } from "vue";
+export default defineComponent({});
+</script>
