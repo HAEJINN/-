d8c7bb4b273c14a15c4d174f28c3b621448945ba
진해진 <j__0308@naver.com>
2021-09-29 19:42:02 +0900

    [S05P21C201][FE] fix : 주소변경

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -3,12 +3,12 @@ module.exports = {
     https: false,
     port: 8083,
     open: true,
-    proxy: {
-      "/api": {
-        target: "http://localhost:8080/",
-        // target: "http://j5c201.p.ssafy.io/",
-      },
-    },
+    // proxy: {
+    //   "/api": {
+    //     target: "http://localhost:8080/",
+    //     // target: "http://j5c201.p.ssafy.io/",
+    //   },
+    // },
     historyApiFallback: true,
     hot: true,
   },
