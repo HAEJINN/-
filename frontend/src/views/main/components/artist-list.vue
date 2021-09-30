ff02a687fa081f64f845d8a26b25e45a340fc0d0
진해진 <j__0308@naver.com>
2021-09-30 23:06:47 +0900

    [S05P21C201][FE] fix : 컴포넌트 정리

--- frontend/src/views/main/components/artist-list.vue
+++ frontend/src/views/main/components/artist-list.vue
@@ -2,30 +2,20 @@
   <div class="q-my-xl">
     <div class="text-center text-h5">최근 가입한 사진가</div>
     <div class="row">
-      <div class="col-lg-3 col-xs-6">
-        <q-avatar class="q-my-md" color="primary" size="120px">
-          <img src="../../../assets/profile.png" />
-        </q-avatar>
-        <div>김작가</div>
-      </div>
-      <div class="col-lg-3 col-xs-6">
-        <q-avatar class="q-my-md" color="primary" size="120px">
-          <img src="../../../assets/profile.png" />
-        </q-avatar>
-        <div>김작가</div>
-      </div>
-      <div class="col-lg-3 col-xs-6">
-        <q-avatar class="q-my-md" color="primary" size="120px">
-          <img src="../../../assets/profile.png" />
-        </q-avatar>
-        <div>김작가</div>
-      </div>
-      <div class="col-lg-3 col-xs-6">
-        <q-avatar class="q-my-md" color="primary" size="120px">
-          <img src="../../../assets/profile.png" />
-        </q-avatar>
-        <div>김작가</div>
-      </div>
+      <ArtistComp></ArtistComp>
+      <ArtistComp></ArtistComp>
+      <ArtistComp></ArtistComp>
+      <ArtistComp></ArtistComp>
     </div>
   </div>
 </template>
+<script>
+import { defineComponent } from "vue";
+import ArtistComp from "../components/artist-comp.vue";
+export default defineComponent({
+  name: "artist-list",
+  components: {
+    ArtistComp,
+  },
+});
+</script>
---
+++ frontend/src/views/main/components/item-comp.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="my-card col-lg-3 col-xs-6">
+    <q-card class="q-ma-sm">
+      <img src="https://cdn.quasar.dev/img/mountains.jpg" />
+
+      <q-card-section>
+        <div class="text-h6">작품 이름</div>
+        <div class="text-subtitle2">작가</div>
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
