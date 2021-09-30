20a74fdf725d16353f079170998db3b7a7f7b100
진해진 <j__0308@naver.com>
2021-09-30 22:35:50 +0900

    [S05P21C201][FE] fix : 사진업로드

--- frontend/src/views/mypage/addpic.vue
+++ frontend/src/views/mypage/addpic.vue
@@ -1,52 +1,62 @@
 <template>
   <div>
-    <div class="column items-center">
-      <div class="text-h5 q-my-md">사진 올리기</div>
+    <div>
+      <div class="col-12 colunm items-center bg-accent q-py-lg q-mb-lg">
+        <div class="text-h5 q-my-md">사진 올리기</div>
+        <div class="q-my-sm">올리고 싶은 사진을 올려주세요</div>
+        <div class="q-my-sm">
+          사진의 제목과 사진에 대한 간단한 설명이 필요합니다
+        </div>
+        <div class="q-my-sm">원하시는 상품의 판매가도 적어주세요</div>
+      </div>
       <div>
-        <label for="name">사진제목</label>
+        <label for="name" class="q-mr-sm">사진제목</label>
         <input
           type="text"
           id="name"
-          placeholder="사진제목"
+          placeholder="제목 입력"
           v-model="state.picture.name"
         />
       </div>
-      <div>
-        <label for="description">사진설명</label>
-        <input
+      <div class="row items-center justify-center q-my-md">
+        <label for="description" class="q-mr-sm">사진설명</label>
+        <textarea
           type="text"
           id="description"
-          placeholder="사진설명"
+          placeholder="사진에 대한 설명 작성"
+          rows="3"
           v-model="state.picture.description"
         />
       </div>
-      <div>
-        <label for="price">가격</label>
+      <div class="q-my-md">
+        <label for="price" class="q-mr-sm">사진가격</label>
         <input
           type="number"
           id="price"
-          placeholder="가격"
+          placeholder="원하는 판매가격"
           v-model="state.picture.price"
         />
       </div>
-      <div>
-        <label for="chooseFile">사진첨부</label>
+      <div class="q-mt-lg q-mb-md">
+        <label for="chooseFile" class="upload-btn q-mr-sm">사진첨부</label>
         <input
           type="file"
           id="chooseFile"
           name="chooseFile"
           accept="image/*"
           @change="loadf"
+          style="display: none"
         />
       </div>
-      <div>
+      <div class="q-my-sm">
         <img src="" class="preview" />
       </div>
-      <button @click="clickUpload()">올리기</button>
+      <button @click="clickUpload()" class="q-mt-xs q-mb-xl">올리기</button>
     </div>
   </div>
 </template>
 <script>
+import "../../styles/mypage.scss";
 import { defineComponent } from "vue";
 import { reactive } from "vue";
 import { useStore } from "vuex";
@@ -69,8 +79,6 @@ export default defineComponent({
       var file = document.getElementById("chooseFile");
       var preview = document.querySelector(".preview");
       preview.src = URL.createObjectURL(file.files[0]);
-
-      console.log(file.files[0]);
 
       preview.style.width = "60%";
       preview.style.height = "60%";
