71a14100ae8598f1290c8140c870d8e1ad8b7601
진해진 <j__0308@naver.com>
2021-10-01 11:14:01 +0900

    [S05P21C201][FE] feat : 사진 모아보기

---
+++ frontend/src/views/banner/components/picture-comp.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="my-card col-lg-3 col-xs-6">
+    <q-card class="q-ma-sm">
+      <img src="../../../assets/mypage/malang.png" />
+      <div class="q-py-sm">
+        <div class="text-h6">사진 이름</div>
+        <div class="text-subtitle2">사진가</div>
+      </div>
+    </q-card>
+  </div>
+</template>
