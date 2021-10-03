faaa16f7b490c5eec6d151842c204c714d6c0644
xrl0603 <xrl0603@naver.com>
2021-10-04 01:34:50 +0900

    [S05P21C201][FE] fix : 로그인/회원가입 css 수정

--- frontend/src/views/login/register.vue
+++ frontend/src/views/login/register.vue
@@ -4,59 +4,75 @@
       view="lhh LpR lff"
       container
       style="height: 500px"
-      class="bg-secondary"
+      class="register-layout bg-secondary"
     >
       <q-page-container>
-        <q-header class="bg-black">
+        <q-header class="bg-dark">
           <q-toolbar>
+            <q-toolbar-title></q-toolbar-title>
             <q-btn v-close-popup flat round dense icon="close" />
           </q-toolbar>
         </q-header>
         <q-page class="column justify-center">
-          <div class="column justify-center items-center">
-            <span class="text-h4 q-my-md">회원가입</span>
-            <div class="row q-my-sm">
-              <label class="q-mr-md" for="email">이메일</label>
-              <input
-                type="text"
-                class="form-control"
-                id="email"
-                v-model="state.user.email"
-                placeholder="이메일"
-              />
+          <div class="row justify-center items-center">
+            <span class="text-h4 q-mb-lg">회원가입</span>
+            <div class="row justify-center">
+              <div class="col-9">
+                <div class="row justify-center items-center q-my-sm">
+                  <label class="col-3" for="email">이메일</label
+                  ><input
+                    class="col-6"
+                    type="text"
+                    id="email"
+                    v-model="state.user.email"
+                    placeholder="이메일"
+                  />
+                </div>
+              </div>
+              <div class="col-9">
+                <div class="row justify-center items-center q-my-sm">
+                  <label class="col-3" for="name">이름</label
+                  ><input
+                    class="col-6"
+                    type="text"
+                    id="name"
+                    v-model="state.user.name"
+                    placeholder="이름"
+                  />
+                </div>
+              </div>
+              <div class="col-9">
+                <div class="row justify-center items-center q-my-sm">
+                  <label class="col-3" for="password">비밀번호</label
+                  ><input
+                    class="col-6"
+                    type="password"
+                    id="password"
+                    v-model="state.user.password"
+                    placeholder="비밀번호"
+                  />
+                </div>
+              </div>
+              <div class="col-9">
+                <div class="row justify-center items-center q-my-sm">
+                  <label class="col-3" for="password-confirm"
+                    >비밀번호 확인</label
+                  ><input
+                    class="col-6"
+                    type="password"
+                    id="password-confirm"
+                    v-model="state.user.passwordConfirm"
+                    placeholder="비밀번호 확인"
+                  />
+                </div>
+              </div>
             </div>
-            <div class="row q-my-sm">
-              <label class="q-mr-md" for="name">이름</label>
-              <input
-                type="text"
-                id="name"
-                v-model="state.user.name"
-                placeholder="이름"
-              />
-            </div>
-            <div class="row q-my-sm">
-              <label class="q-mr-md" for="password">비밀번호</label>
-              <input
-                type="password"
-                id="password"
-                v-model="state.user.password"
-                placeholder="비밀번호"
-              />
-            </div>
-            <div class="row q-my-sm">
-              <label class="q-mr-md" for="password-confirm"
-                >비밀번호 확인</label
-              >
-              <input
-                type="password"
-                id="password-confirm"
-                v-model="state.user.passwordConfirm"
-                placeholder="비밀번호 확인"
-              />
-            </div>
-            <button class="q-my-lg" type="submit" @click="clickRegister()">
-              회원가입
-            </button>
+            <q-btn
+              class="bg-dark q-mt-lg"
+              text-color="white"
+              label="회원가입"
+              @click="clickRegister()"
+            />
           </div>
         </q-page>
       </q-page-container>
@@ -65,7 +81,7 @@
 </template>
 
 <script>
-import "../../styles/login.scss";
+import "../../styles/register.scss";
 import { defineComponent, reactive } from "vue";
 import { useStore } from "vuex";
 import { useRouter } from "vue-router";
