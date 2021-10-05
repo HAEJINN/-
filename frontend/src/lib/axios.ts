f5aad6aab42d1e8c38828100519b3a47c1deadec
이호황 <ghghkd52@naver.com>
2021-10-05 06:02:19 +0000

    Update axios.ts

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -3,7 +3,7 @@ import axios, { AxiosInstance } from "axios";
 const instance: AxiosInstance = axios.create({
   // baseURL: "/api",
   // baseURL: "http://localhost:8080",
-  baseURL: "http://j5c201.p.ssafy.io"
+  baseURL: "http://j5c201.p.ssafy.io",
   headers: {
     "Content-type": "application/json",
   },
