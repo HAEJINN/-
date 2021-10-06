b9c9ca42f74ca7981372f79ba237dac2e7409624
hohwang <ghghkd52@naver.com>
2021-10-06 16:36:32 +0900

    [S05P21C201-184][BE] fix: NFT request 수정

--- backend/src/main/java/com/ecommerce/domain/nft/domain/NftRequest.java
+++ backend/src/main/java/com/ecommerce/domain/nft/domain/NftRequest.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class NftRequest {
     private Long userId;
-    private String CID;
+    private String cid;
     private String walletAddress;
     private String name;
     private String description;
