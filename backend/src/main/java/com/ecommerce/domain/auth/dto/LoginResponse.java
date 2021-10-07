0557d84a242977a8610c69512e369124a466643e
김우재 <kwj1270@naver.com>
2021-10-07 18:34:38 +0900

    fix: http 메서드 변경

--- backend/src/main/java/com/ecommerce/domain/auth/dto/LoginResponse.java
+++ backend/src/main/java/com/ecommerce/domain/auth/dto/LoginResponse.java
@@ -16,6 +16,7 @@ public class LoginResponse implements Serializable {
 
     private Long id;
     private String email;
+    private String profileImage;
     private String name;
     private String jwtToken;
     private UserStatus status;
@@ -24,6 +25,7 @@ public class LoginResponse implements Serializable {
     public LoginResponse(final User user, final String jwtToken) {
         this.id = user.getId();
         this.email = user.getEmail();
+        this.profileImage = user.getProfileImage();
         this.name = user.getName();
         this.jwtToken = jwtToken;
         this.status = user.getStatus();
